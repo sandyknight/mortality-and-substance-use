6d3ba30149485dc2343609ad8e25501bd0f442ea
Knight <Sandy.Knight@dhsc.gov.uk>
2024-11-09 10:25:53 +0000

    change to focus on Ask 1 first

--- R/README.md
+++ R/README.md
@@ -6,6 +6,30 @@
 
 ## 1. Proportion of deaths
 
+Original email instructions from JK:
+
+> Is deaths during and after treatment that not poisonings (or alc specific) but will be in a large part due to drug or alcohol use. So once you have worked out the total number of poisoning deaths 
+
+> Using the data in the toolkit on the beta site can you add in the other opiate deaths of other causes not poisoning for those that died in treatment in the year you are looking at. Then the same for non-opiates and lastly the same for alcohol excluding alc specific deaths that you will have got from the alc specific data– you should be able to get the age for all of these which is why you need to use the beta site. 
+
+> Can you also then add in the people that have died one year after leaving treatment – using the year before and selecting the treatment status of one year post (we can assume that these are also all drug and alc related being so close to their treatment date) 
+
+> Can you split out each different cohort and component you are adding so we can see how much each one adds. 
+
+> Lastly with we will need to then apply similar rates of all cause mortality to opiate users outside of treatment but lets do all the above first and then discuss. 
+  
+
+Latest email instructions from JK:
+
+> Morning – this has gone in for the 18th. If possible it would be really good to have some of the premature mortality stuff available to discuss at the meeting. So could you focus first on updating the work that estimates the total number of drug and alcohol deaths by age (with drugs separate, alcohol separate and then combined) and then what % of total deaths in each age range (and overall under 50) are a result of either of the three substance group above. 
+
+> It would also be good to have an overall breakdown of how we have arrived at the total number of drug and alcohol deaths. i.e. DRDs + non DRD drug users in treatment + non DRDs drugs users left treatment but died in the year following etc. 
+
+> Lastly if there is time would be good to have the outputs by deprivation
+
+> Hopefully you have all you need for this but happy to dig out some of the original specs if helpful. Let me know. 
+
+> If you can finish this in time then can look at LYL but I think the above is probably more useful for that conversation. 
 
 
 
---
+++ R/get_data.R
@@ -0,0 +1,130 @@
+library(testthat)
+library(data.table)
+library(tidyverse)
+library(openxlsx)
+library(arrow)
+
+
+
+
+# IMD ---------------------------------------------------------------------
+
+url <- # UTLA IMD summaries URL
+  "https://assets.publishing.service.gov.uk/media/5d8b3d7aed915d0369518030/File_11_-_IoD2019_Local_Authority_District_Summaries__upper-tier__.xlsx"
+
+imd <-
+read.xlsx(xlsxFile = url, sheet = "IMD") |>
+  janitor::clean_names()
+
+imd |> 
+  select(upper_tier_local_authority_district_code_2019, upper_tier_local_authority_district_name_2019, imd_average_score) |>
+  mutate(imd_decile = ntile(imd_average_score, 10)) |> 
+  write_csv("data/raw/utla_imd_decile.csv")
+
+
+# Drug poisoning deaths ---------------------------------------------------
+
+#  This file is NDTMS-ONS data linkage; received by email from Stefan and named:
+# "table1_all deaths_Cocaine version 1.xlsx"
+
+df <- # Load deaths data
+  openxlsx::read.xlsx("data/raw/table1_all deaths_Cocaine version 1.xlsx", sheet = "table1_all deaths")
+
+write_parquet(df, "data/raw/ndtms_mortality_data.parquet")
+
+# dt <- # Deaths from drug misuse & suspected deaths from drug misuse but not recorded as such
+#   dt[(drug_misuse_combined == 0 & Treatment_Status != "no match with NDTMS")| drug_misuse_combined == 1,]
+# 
+# write_parquet(dt, "data/raw/deaths_related_to_misuse_inc_not_rec.parquet")
+
+
+poisoning_deaths_ons <- read_parquet("data/raw/ndtms_mortality_data.parquet")
+
+poisoning_deaths_ons |> 
+  mutate(ndtms_match = if_else(Treatment_Status == "no match with NDTMS", "Record of contact with treatment system", "No record of contact with treatment system")) |> 
+  filter(drug_group == "Total Deaths") |> 
+  mutate(ons_misuse = if_else(drug_misuse_combined == 1, "Recorded as drug misuse by ONS", "Not recorded as drug misuse by ONS")) |> 
+  group_by(reg_year, ndtms_match, ons_misuse) |> 
+  summarise(n = n()) |> 
+  filter(reg_year > 2022) |> 
+  pivot_wider(names_from = ndtms_match, values_from = n) |> 
+  ungroup() |> 
+  select(-reg_year) |> 
+  janitor::adorn_totals(where = c("row", "col"))
+
+
+
+# Non-poisoning deaths ----------------------------------------------------
+
+# Data received by email from mailto:Chioma.Amasiatu@dhsc.gov.uk, Mon 08/07/2024 10:28 and named `post election data for Jon- sent.xlsx`
+
+tx_deaths <- 
+  openxlsx::read.xlsx("data/raw/post election data for Jon- sent.xlsx", sheet = "NDTMS_ONS") |> 
+  janitor::clean_names()
+
+england_tx_deaths <- 
+tx_deaths |> 
+  filter(area_name == "England")
+
+tx_deaths <- 
+tx_deaths |> 
+  filter(geography == "LA")
+
+la_count <- 
+tx_deaths |> 
+  filter(period_range == "April 2022 to March 2023") |> 
+  group_by(agegrp, death_cause) |> 
+  summarise(count = sum(count)) |> 
+  pivot_wider(names_from = agegrp, values_from = count, values_fill = 0)
+
+england_count <- 
+england_tx_deaths |> 
+  filter(period_range == "April 2022 to March 2023") |> 
+  group_by(agegrp, death_cause) |> 
+  summarise(count = sum(count)) |> 
+  pivot_wider(names_from = agegrp, values_from = count, values_fill = 0)
+
+
+test_that(
+ "Sum of LAs = England total",{ expect_equal(la_count, england_count)}
+)
+rm(la_count);rm(england_count)
+
+
+tx_deaths |> 
+  select(treatment_status) |> unique()
+
+#addtional_drd <- 
+tx_deaths |> 
+  filter(period_range == "April 2022 to March 2023") |> 
+  filter(death_cause != "Drug poisoning") |> 
+  filter(death_cause != "Alcohol-specific death") |> 
+  filter(drug_group  != "alcohol only") |> 
+  as_tibble() |> 
+  mutate(treatment_status = factor(treatment_status, levels = c("Died in treatment", "Died within a year of discharge", "Died one or more years following discharge"))) |> 
+  group_by(treatment_status, death_cause, agegrp) |> 
+  summarise(count = sum(count)) 
+ # pivot_wider(names_from = treatment_status, values_from = count, values_fill = 0) |> 
+  janitor::tabyl(agegrp, death_cause, treatment_status)
+
+addtional_drd <- 
+tx_deaths |> 
+  filter(period_range == "April 2022 to March 2023") |> 
+  filter(death_cause != "Drug poisoning") |> 
+  filter(death_cause != "Alcohol-specific death") |> 
+  filter(drug_group  != "alcohol only") |> 
+  filter(treatment_status != "Died one or more years following discharge") |> 
+  as_tibble() |> 
+  group_by(area_code, area_name, death_cause, age, agegrp, drug_group, treatment_status) |> 
+  summarise(count = sum(count)) |> 
+  pivot_wider(names_from = treatment_status, values_from = count, values_fill = 0) 
+
+
+
+
+
+
+
+
+
+
