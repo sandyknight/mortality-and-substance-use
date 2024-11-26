db2c0635c402d06ee7a170b71c964cd6a4a5d845
sandyknight <sandyknight90@gmail.com>
2024-11-26 13:24:34 +0000

    alcohol script start

--- R/alcohol_deaths_functions.R
+++ R/alcohol_deaths_functions.R
@@ -7,16 +7,61 @@ tx_alcohol_deaths <-
 alc_spec_deaths <-
   read_csv("data/processed/ons_alcohol-specific_deaths.csv")
 
-alc_spec_deaths |> 
-  group_by(individual_cause_of_death_description) |> 
-  summarise(n = sum(n), .groups = "drop") |> 
-  arrange(-n) |> 
-  filter(n > 0) |> 
-  write_csv("data/processed/ons_alcohol-specific_deaths_by_cause.csv")
+alc_spec_deaths_by_cause <-
+alc_spec_deaths |>
+  group_by(individual_cause_of_death_description) |>
+  summarise(n = sum(n), .groups = "drop") |>
+  arrange(-n) |>
+  filter(n > 0)
+
+tx_alcohol_deaths_by_cause <-
+tx_alcohol_deaths |>
+  group_by(death_cause) |>
+  summarise(count  = sum(count), .groups = "drop") |>
+  arrange(-count)
+
+total_alcohol_deaths <-
+tx_alcohol_deaths_by_cause %>%
+  mutate(count = case_when(death_cause == "Alcohol-specific death" ~ sum(alc_spec_deaths$n),
+                   TRUE ~ count)) %>%
+  mutate(death_category = if_else(
+    death_cause == "Alcohol-specific death",
+    "Alcohol-specific deaths (ONS)",
+    "Additional deaths (NDTMS-ONS linkage)"
+    ))
 
 
-tx_alcohol_deaths |> 
-  group_by(death_cause) |> 
-  summarise(count  = sum(count), .groups = "drop") |> 
-  arrange(-count) |> 
-  write_csv("data/processed/tx_alcohol_deaths_by_cause.csv")
+
+total_alcohol_deaths <-
+total_alcohol_deaths %>%
+  group_by(death_category) %>%
+  summarise(count = sum(count), .groups = "drop")
+
+
+total_alcohol_deaths %>%
+  arrange(count) %>%
+  mutate(death_category = as_factor(death_category)) %>%
+  ggplot(aes(x = 1, y = count)) +
+  geom_col(aes(fill = death_category), colour = "black") +
+  my_theme +
+  theme(legend.position = "none") +
+  labs(x = NULL, y = "Deaths (n)") +
+  scale_y_continuous(labels = scales::comma) +
+  scale_x_continuous(limits = c(0,3))
+  scale_fill_dhsc()
+
+tx_alcohol_deaths_by_cause %>%
+  filter(death_cause != "Alcohol-specific death") %>%
+  arrange(count) %>%
+  mutate(death_cause = as_factor(death_cause)) %>%
+  ggplot(aes(x = 1, y = count)) +
+  geom_col(aes(fill = death_cause), colour = "black") +
+  my_theme +
+  theme(legend.position = "right") +
+  labs(x = NULL, y = "Deaths (n)") +
+  scale_y_continuous(labels = scales::comma) +
+  scale_x_continuous(limits = c(0,3))
+
+scale_fill_dhsc()
+
+
